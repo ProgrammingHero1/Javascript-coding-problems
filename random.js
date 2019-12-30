d43cd5d692850de159aaf293bb04f24e10149da9
That JS Dude <khan4019@gmail.com>
2019-12-29 18:37:37 -0800

    random

---
+++ random.js
@@ -0,0 +1,8 @@
+var age = 17;
+var price = 3.991;
+var rounded = Math.floor(price);
+for(var i = 0; i<10; i++){
+    var random = Math.random();
+    var floored = Math.round(random*100);
+    console.log(floored);
+}
